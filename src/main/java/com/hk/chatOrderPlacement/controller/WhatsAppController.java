25da87194aba4bc7dee6864836dbb8270c91490f
dhruvnagpalblc <dhruv.nagpal@Brightlifecare.com>
2024-02-07 21:19:59 +0530

    fix

--- src/main/java/com/hk/chatOrderPlacement/controller/WhatsAppController.java
+++ src/main/java/com/hk/chatOrderPlacement/controller/WhatsAppController.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class WhatsAppController {
 
-    @PostMapping("/sendMessage")
+    @RequestMapping(value = "/sendMessage", method = RequestMethod.POST)
     public ResponseEntity<String> sendMessage() {
 
         try {
